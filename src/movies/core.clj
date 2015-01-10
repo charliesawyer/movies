ccde076da97e9e2f48e8a32d7d44013fe06ab14e
Tom Lyons <tbl3rd@gmail.com>
2015-01-10 13:37:03 -0500

    make-n-zeros

--- src/movies/core.clj
+++ src/movies/core.clj
@@ -1,6 +1,21 @@
 (ns movies.core
   (:gen-class))
 
+(defn zeros
+  [n]
+  (vec (repeat n 0)))
+
+(defn zeros
+  [n]
+  (if (zero? n) []
+      (conj (zeros (- n 1)) 0)))
+
+(defn zeros
+  [n]
+  (loop [result [] n n]
+    (if (zero? n) result
+        (recur (conj result 0) (- n 1)))))
+
 (defn -main
   []
   (println "Hello world!"))
