19298b6d6c298cf7d505441d1545a3d321792b3a
Tom Lyons <tbl3rd@gmail.com>
2015-01-12 19:29:51 -0500

    TIL

--- src/movies/core.clj
+++ src/movies/core.clj
@@ -16,6 +16,18 @@
     (if (zero? n) result
         (recur (conj result 0) (- n 1)))))
 
+(defn zeros
+  [n]
+  (nth (iterate #(conj % 0) []) n))
+
+(zeros 7)
+
 (defn -main
   []
   (println "Hello world!"))
+
+(mapv (fn [v] (* 2 v)) (vec (range 1 9)))
+;;=> [2 4 6 8 10 12 14 16]
+
+(reduce-kv (fn [r k v] (conj r [k v])) [] (vec (range 1 9)))
+;;=> [[0 1] [1 2] [2 3] [3 4] [4 5] [5 6] [6 7] [7 8]]
