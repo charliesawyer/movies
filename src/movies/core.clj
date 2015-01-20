d02fa3ffce1e97c462d0bf5cd2402775eb0ec52a
Tom Lyons <tbl3rd@gmail.com>
2015-01-20 18:04:33 -0500

    Help Jack.

--- src/movies/core.clj
+++ src/movies/core.clj
@@ -31,3 +31,18 @@
 
 (reduce-kv (fn [r k v] (conj r [k v])) [] (vec (range 1 9)))
 ;;=> [[0 1] [1 2] [2 3] [3 4] [4 5] [5 6] [6 7] [7 8]]
+
+(defn fizzbuzzer
+  [n]
+  (let [fb (fn [d s] (if (zero? (mod n d)) s ""))
+        s (str (fb 3 "Fizz") (fb 5 "Buzz"))]
+    (if (empty? s) n s)))
+
+(map fizzbuzzer (range 1 100))
+
+(defn x+1 [x]  (+ x 1))
+(defn x*3 [x] (* x 3))
+
+(macroexpand '(x*3 (-> 5 x+1))) ;;=> (x*3 (-> 5 x+1))
+
+(macroexpand '(-> 5 (fn [x] (+ x 1)) x*3)) ;;=> (x*3 (fn 5 [x] (+ x 1)))
