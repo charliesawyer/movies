285651326cb01341b6ebf36d5fe984d56717bb5a
Tom Lyons <tbl3rd@gmail.com>
2015-02-15 10:10:42 -0500

    Add Guava dependency.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.google.guava/guava "18.0"]]
   :main movies.core
   :profiles {:uberjar {:aot :all}})
