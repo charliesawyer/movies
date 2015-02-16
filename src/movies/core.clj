cc9d6f04715329a98c27872d53f5a9d69da371b3
Charles Sawyer <charlessawyer@ts-macbook-pro.home>
2015-02-16 11:05:30 -0500

    Adding function to add a movie to the db's array for the movie's year

--- src/movies/core.clj
+++ src/movies/core.clj
@@ -39,13 +39,48 @@
 
 (movies-from-year my-vids "1941")
 
-{:title "Citize Kane" :year 1941}
-{:title "Jaws" :year 1970}
-{:year 1941 [{:title "Citizen Kane" :year 1941} {:title "Suspense" :year 1941}]}
-{:year 1970 [{:title "foo" :year 1970} {:title "Jaws" :year 1970}]}
-
 {"1941" [{:title "Citizen Kane" :year "1941"}
          {:title "Suspense" :year "1941"}]
  "1970" [{:title "Jaws" :year "1970"}]}
 
-(get {1941 "foo"} 1941)
+(defn make-movie-database
+  "Turn stream of movies ms into a database index by year"
+  [ms]
+  (let [movie (first ms)]
+    {(:year movie) [movie]}))
+
+(make-movie-database my-vids)
+(conj [{:title "Citizen Kane" :year "1941"}
+       {:title "Suspense" :year "1941"}]
+      {:title "Jaws" :year "1970"})
+(def mini-db {"1941" [{:title "Citizen Kane" :year "1941"}
+         {:title "Suspense" :year "1941"}]
+ "1970" [{:title "Jaws" :year "1970"}]}
+)
+(conj {:foo 1 :bar 2} {:foo 3 :hoodoo "foo"})
+(def x {:foo [1 2 3] :bar "bar"})
+(update-in x [:foo] conj 4)
+(def psycho {:year "1970" :title "Psycho"})
+(update-in mini-db ["1970"] conj psycho)
+(get mini-db "1941")
+
+(defn make-empty-movie-db []
+  (loop [iteration 2010 ray {}]
+    (if (> iteration 2015)
+      ray
+      (recur (inc iteration) (conj ray {(str iteration) []})))))
+(make-empty-movie-db)
+(def movie-db (make-empty-movie-db))
+movie-db
+
+(defn add-movie-to-db-entry [db movie]
+  "Add movie to array for movie's year"
+  (let [year (:year movie)]
+     (conj (or (get db year) []) movie)))
+
+(def mv1 {:year "1941" :title "Citizen Kane"})
+(add-movie-to-db {} mv) ;; Should return [mv1], returns {}
+(def mv2 {:year "1941" :title "Suspense"})
+(add-movie-to-db (add-movie-to-db {} mv1) mv2) ;; should return [mv1
+;; mv2], returns {} instead
+
